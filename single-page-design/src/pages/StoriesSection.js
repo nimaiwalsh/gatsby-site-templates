2fd88e604204159f295b0d6ce6a92e405770224f
nimaiwalsh <nimai.walsh@gmail.com>
2018-08-07 19:25:33 +1000

    created StoryCard component

--- single-page-design/src/pages/StoriesSection.js
+++ single-page-design/src/pages/StoriesSection.js
@@ -6,6 +6,7 @@ import StoryCard from '../components/StoryCard'
 import Section from './StoriesSection.css'
 
 import Image1 from '../resources/images/nat-8.jpg'
+import Image2 from '../resources/images/nat-9.jpg'
 
 const StoriesSection = () => {
   return (
@@ -18,6 +19,14 @@ const StoriesSection = () => {
           image={Image1}
           caption="Mary Smith"
           heading="I had the best weekend with my family"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet tempora repellendus temporibus, quidem facilis dolorum dolor. Optio quibusdam sint voluptatem id veritatis. Exercitationem esse non incidunt fugiat tempora nisi doloribus!"
+        />
+      </Row>
+      <Row>
+        <StoryCard
+          image={Image2}
+          caption="Jack Citizen"
+          heading="I feel so much more energised"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet tempora repellendus temporibus, quidem facilis dolorum dolor. Optio quibusdam sint voluptatem id veritatis. Exercitationem esse non incidunt fugiat tempora nisi doloribus!"
         />
       </Row>
